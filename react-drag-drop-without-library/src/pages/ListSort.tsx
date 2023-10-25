e5936fa766f4aee93b444efa8bd7f44b320d9382
Rohan Sharma <onlyrohan21@gmail.com>
2023-10-26 02:18:01 +0530

    list-sort update

--- react-drag-drop-without-library/src/pages/ListSort.tsx
+++ react-drag-drop-without-library/src/pages/ListSort.tsx
@@ -1,91 +1,232 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Select from "react-select";
+import axios from "axios";
 
-function ListSort() {
-  const [skillItems, setSkillItems] = React.useState([
-    "Add a Skill"
-  ]);
-  const [newSkillItem, setNewSkillItem] = React.useState("");
-  const [showInput, setShowInput] = React.useState(true);
+type ListItem = string;
 
-  //save reference for dragItem and dragOverItem
+function App() {
+  const initialItem = "Add a skill";
+
+  const [leftList, setLeftList] = useState<Array<ListItem>>(
+    Array(5).fill(initialItem)
+  );
+  const [rightList, setRightList] = useState<Array<ListItem>>(
+    Array(5).fill(initialItem)
+  );
+  const [options, setOptions] = useState<any[]>([]);
+
+  const [leftSelectedOptions, setLeftSelectedOptions] = useState<any[]>(
+    Array(5).fill(null)
+  );
+  const [rightSelectedOptions, setRightSelectedOptions] = useState<any[]>(
+    Array(5).fill(null)
+  );
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow"
+      )
+      .then((response: any) => {
+        const options = response.data.items.map((item: any) => ({
+          value: item.name,
+          label: item.name,
+        }));
+        setOptions(options);
+      })
+      .catch((error: any) => {
+        console.error("Error fetching options:", error);
+      });
+  }, []);
+
+  const handleItemClick = (
+    list: Array<ListItem>,
+    setList: (list: Array<ListItem>) => void,
+    index: number
+  ) => {
+    const updatedList = [...list];
+    updatedList[index] = initialItem; // Make the item editable
+    setList(updatedList);
+  };
+  //   --------------- focus lost--------------
+  const handleBlur = (
+    list: Array<ListItem>,
+    setList: (list: Array<ListItem>) => void,
+    index: number,
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const updatedList = [...list];
+    if (list === rightList) {
+      if (e.target.value === "") updatedList[index] = initialItem;
+      else updatedList[index] = e.target.value;
+      setList(updatedList);
+    } else {
+      if (e.target.value === "") updatedList[index] = initialItem;
+      else updatedList[index] = e.target.value;
+      setList(updatedList);
+    }
+  };
+
+  //   --------------- clear btn func--------------
+  const handleClearClick = (
+    list: Array<ListItem>,
+    setList: (list: Array<ListItem>) => void,
+    index: number
+  ) => {
+    const updatedList = [...list];
+    updatedList[index] = initialItem;
+    setList(updatedList);
+  };
+
   const dragItem = React.useRef<any>(null);
   const dragOverItem = React.useRef<any>(null);
 
-  //const handle drag sorting
-  const handleSort = () => {
+  //   --------------- drag func--------------
+  const handleSort = (
+    list: Array<ListItem>,
+    setList: (list: Array<ListItem>) => void,
+    index: number
+  ) => {
     //duplicate items
-    let _skillItems = [...skillItems];
+    let _skillItems = [...list];
 
     //remove and save the dragged item content
-    const draggedItemContent = _skillItems.splice(dragItem.current, 1)[0];
+    let draggedItemContent = _skillItems.splice(dragItem.current, 1)[0];
 
-    //switch the position
     _skillItems.splice(dragOverItem.current, 0, draggedItemContent);
-
     //reset the position ref
     dragItem.current = null;
     dragOverItem.current = null;
-
     //update the actual array
-    setSkillItems(_skillItems);
+    setList(_skillItems);
   };
 
-  //handle name change
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNewSkillItem(e.target.value);
+  // fetched data
+  const [data, setData] = useState<any[]>([]);
+  const [filterData, setFilterData] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch(
+      "https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow"
+    )
+      .then((res) => res.json())
+      .then((fetchedData) => {
+        setData(fetchedData.items);
+        setFilterData(fetchedData.items);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleFilter = (value: string) => {
+    const res = filterData.filter((f) => f.name.toLowerCase().includes(value));
+    setData(res);
   };
 
-  //handle new item addition
-  const handleAddItem = () => {
-    const _skillItems = [...skillItems];
-    _skillItems.push(newSkillItem);
-    setSkillItems(_skillItems);
-    setShowInput(false);
+  const handleLeftOptionChange = (
+    index: number,
+    selectedOption: any | null
+  ) => {
+    const updatedOptions = [...leftSelectedOptions];
+    updatedOptions[index] = selectedOption;
+    setLeftSelectedOptions(updatedOptions);
+  };
+
+  const handleRightOptionChange = (
+    index: number,
+    selectedOption: any | null
+  ) => {
+    const updatedOptions = [...rightSelectedOptions];
+    updatedOptions[index] = selectedOption;
+    setRightSelectedOptions(updatedOptions);
   };
 
   return (
-    <div className="app">
-      <h2>Skill List</h2>
-
-      <div className="input-group">
-        {/* <input
-          type="text"
-          name="fruitName"
-          placeholder="e.g Banana"
-          onChange={handleNameChange}
-        /> */}
-        <button className="btn" onClick={handleAddItem}>
-          Add item
-        </button>
-      </div>
-
-      <div className="list-sort">
-        {skillItems.map((item, index) => (
-          <div
-            key={index}
-            className="list-item"
-            draggable
-            onDragStart={(e) => (dragItem.current = index)}
-            onDragEnter={(e) => (dragOverItem.current = index)}
-            onDragEnd={handleSort}
-            onDragOver={(e) => e.preventDefault()}
-          >
-            {/* <input
-          type="text"
-          name="fruitName"
-          placeholder="Add Skill"
-          onChange={handleNameChange}
-          // style={{visibility: showInput ? 'visible' : 'hidden' }} 
-        /> */}
-            
-            <h4>{index}{item}</h4>
-            <i className="fa-regular fa-circle-xmark"></i>
+    <div className="main-Container">
+      <h4>
+        <li>Things you are good at</li>
+      </h4>
+      <div className="App">
+        <h4 className="subHead">
+          The skills you mention here will help organizers in assessing you as a
+          potential participant.
+        </h4>
+        <div className="columns">
+          <div className="left-column">
+            <ul>
+              {leftList.map((item, index) => (
+                <li
+                  draggable
+                  onDragStart={(e) => (dragItem.current = index)}
+                  onDragEnter={(e) => (dragOverItem.current = index)}
+                  onDragEnd={(e) => handleSort(leftList, setLeftList, index)}
+                  onDragOver={(e) => e.preventDefault()}
+                  key={index}
+                >
+                  {item === initialItem ? (
+                    <Select
+                      options={options}
+                      value={leftSelectedOptions[index]}
+                      onChange={(selectedOption: any) =>
+                        handleLeftOptionChange(index, selectedOption)
+                      }
+                      isSearchable
+                      isClearable
+                      placeholder={index + 1 + ". " + item}
+                    />
+                  ) : (
+                    index + 1 + ". " + item
+                  )}
+                  <i
+                    className="fa-regular fa-circle-xmark"
+                    onClick={() =>
+                      handleClearClick(leftList, setLeftList, index)
+                    }
+                  ></i>
+                </li>
+              ))}
+            </ul>
           </div>
-        ))}
+          <div className="right-column">
+            <ul>
+              {rightList.map((item, index) => (
+                <li
+                  draggable
+                  onDragStart={(e) => (dragItem.current = index)}
+                  onDragEnter={(e) => (dragOverItem.current = index)}
+                  onDragEnd={(e) =>
+                    handleSort(rightList, setRightList, index + 5)
+                  }
+                  onDragOver={(e) => e.preventDefault()}
+                  key={index}
+                >
+                  {item === initialItem ? (
+                    <Select
+                      options={options}
+                      value={rightSelectedOptions[index]}
+                      onChange={(selectedOption: any) =>
+                        handleRightOptionChange(index, selectedOption)
+                      }
+                      isSearchable
+                      isClearable
+                      placeholder={index + 6 + ". " + item}
+                    />
+                  ) : (
+                    index + 6 + ". " + item
+                  )}
+                  <i
+                    className="fa-regular fa-circle-xmark"
+                    onClick={() =>
+                      handleClearClick(rightList, setRightList, index)
+                    }
+                  ></i>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
-export default ListSort;
-
+export default App;
